339a6ad36cc4ca6d28b495c5347e68a64e060a27
Viery Nugroho <viery15102002@gmail.com>
2024-04-03 08:44:02 +0700

    update: db table relations

--- src/databases/models/auth.js
+++ src/databases/models/auth.js
@@ -1,27 +1,33 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Auth extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Auth.init({
-    id: DataTypes.STRING,
-    userId: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    confirmPassword: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Auth',
-  });
-  return Auth;
+	class Auth extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			Auth.belogsTo(models.User, {
+				foreignKey: {
+					name: 'userId',
+				},
+			});
+		}
+	}
+	Auth.init(
+		{
+			id: DataTypes.STRING,
+			userId: DataTypes.STRING,
+			email: DataTypes.STRING,
+			password: DataTypes.STRING,
+			confirmPassword: DataTypes.STRING,
+		},
+		{
+			sequelize,
+			modelName: 'Auth',
+		}
+	);
+	return Auth;
 };
--- src/databases/models/categoryitem.js
+++ src/databases/models/categoryitem.js
@@ -1,24 +1,31 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class CategoryItem extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  CategoryItem.init({
-    id: DataTypes.STRING,
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'CategoryItem',
-  });
-  return CategoryItem;
+	class CategoryItem extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+
+			CategoryItem.hasMany(models.Item, {
+				foreignKey: {
+					name: 'categoryId',
+				},
+			});
+		}
+	}
+	CategoryItem.init(
+		{
+			id: DataTypes.STRING,
+			name: DataTypes.STRING,
+		},
+		{
+			sequelize,
+			modelName: 'CategoryItem',
+		}
+	);
+	return CategoryItem;
 };
